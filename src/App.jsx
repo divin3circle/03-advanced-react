98e2a8cdbd9355fe2476cbf5216ff40b40ddc06e
Sylus Abel <119014867+divin3circle@users.noreply.github.com>
2023-05-07 22:28:59 +0300

    fetching-data

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 /* import Starter from "./tutorial/01-useState/starter/02-useState-basics"; */
-import Starter from "./tutorial/03-conditional-rendering/starter/01-multiple-returns-basics";
+import Starter from "./tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data";
 
 function App() {
   return (
