983aa4297c43ebcb0ef93bb003a226c582773baa
Sylus Abel <119014867+divin3circle@users.noreply.github.com>
2023-05-07 23:31:32 +0300

    react-short-circuiting

--- src/tutorial/03-conditional-rendering/starter/04-short-circuit-overview.jsx
+++ src/tutorial/03-conditional-rendering/starter/04-short-circuit-overview.jsx
@@ -1,6 +1,16 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const ShortCircuitOverview = () => {
-  return <h2>short circuit overview</h2>;
+  const [firstState, setFirstState] = useState("");
+  const [secondState, setSecondState] = useState("name");
+
+  return (
+    <div>
+      <h4>Falsy OR: {firstState || "Hello World"}</h4>
+      <h4>Falsy AND: {firstState && "Hello World"}</h4>
+      <h4>Truthy OR: {secondState || "Hello World"}</h4>
+      <h4>Truthy AND: {secondState && "Hello World"}</h4>
+    </div>
+  );
 };
 export default ShortCircuitOverview;
