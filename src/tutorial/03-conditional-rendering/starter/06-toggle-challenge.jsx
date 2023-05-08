6cdd058847e13cb1950d7cb74899bff4f5010d11
Sylus Abel <119014867+divin3circle@users.noreply.github.com>
2023-05-08 13:24:28 +0300

    toggle-login-signout-challenge

--- src/tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx
+++ src/tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx
@@ -1,5 +1,44 @@
+import { useState } from "react";
+
 const ToggleChallenge = () => {
-  return <h2>toggle challenge</h2>;
+  const [user, setUser] = useState(null);
+
+  /*   const toggle = () => {
+    toggleAlert ? setToggleAlert(false) : setToggleAlert(true);
+  }; */
+
+  const login = () => {
+    //connect to db or api
+    setUser({ name: "Sylus Abel" });
+  };
+  const logout = () => {
+    setUser(null);
+  };
+
+  return (
+    <div>
+      {user ? (
+        <div>
+          <h4>Hello there, {user.name}</h4>
+          <h5>Congratulations you are logged in</h5>
+          <button className="btn" onClick={logout}>
+            Log out
+          </button>
+        </div>
+      ) : (
+        <div>
+          <h4>Please login</h4>
+          <button className="btn" onClick={login}>
+            Login
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+const Alert = () => {
+  return <div className="alert alert-danger">Hello alert</div>;
 };
 
 export default ToggleChallenge;
