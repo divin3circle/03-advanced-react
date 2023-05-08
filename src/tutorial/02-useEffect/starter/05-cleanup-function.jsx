aa2c77cc6809edd9358cf8c7755c2beae3292824
Sylus Abel <119014867+divin3circle@users.noreply.github.com>
2023-05-08 14:04:43 +0300

    cleaun-up

--- src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
+++ src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
@@ -1,5 +1,30 @@
+import { useState, useEffect } from "react";
+
 const CleanupFunction = () => {
-  return <h2>cleanup function</h2>;
+  const [toggle, setToggle] = useState(false);
+
+  return (
+    <div>
+      <h2 style={{ color: "red" }}>Toggle components</h2>
+      <button className="btn" onClick={() => setToggle(!toggle)}>
+        Toggle
+      </button>
+      {toggle && <AnotherComponent />}
+    </div>
+  );
+};
+
+const AnotherComponent = () => {
+  useEffect(() => {
+    console.log("Another component");
+    const intId = setInterval(() => {
+      console.log("Current");
+    }, 1000);
+    return () => {
+      clearInterval(intId);
+    };
+  }, []);
+  return <h1>Hello world</h1>;
 };
 
 export default CleanupFunction;
