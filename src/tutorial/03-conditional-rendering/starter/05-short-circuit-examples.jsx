7e4a83f6ae198268dd0b3bd8592473f64be19d8a
Sylus Abel <119014867+divin3circle@users.noreply.github.com>
2023-05-08 12:47:26 +0300

    short-circuit

--- src/tutorial/03-conditional-rendering/starter/05-short-circuit-examples.jsx
+++ src/tutorial/03-conditional-rendering/starter/05-short-circuit-examples.jsx
@@ -1,14 +1,47 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const ShortCircuitExamples = () => {
   // falsy
-  const [text, setText] = useState('');
+  const [text, setText] = useState("");
   // truthy
-  const [name, setName] = useState('susan');
-  const [user, setUser] = useState({ name: 'john' });
+  const [name, setName] = useState("susan");
+  const [user, setUser] = useState({ name: "John" });
   const [isEditing, setIsEditing] = useState(false);
 
-  return <h2>short circuit - examples</h2>;
+  return (
+    <div>
+      <h2>{text || "Default value"}</h2>
+      {text && (
+        <div>
+          <h2>Another Return</h2>
+          <h2>{name}</h2>
+        </div>
+      )}
+      {user && <SomeComponent user={user} />}
+      <h2 style={{ margin: "1rem" }}>Tenary Operator</h2>
+      <button className="btn">{isEditing ? "Edit" : "Add"}</button>
+      {!user ? (
+        <div>
+          <h4>Hello there user {user.name}</h4>
+        </div>
+      ) : (
+        <div>
+          <h4>Please log in</h4>
+          <button className="btn">Log In</button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+const SomeComponent = (props) => {
+  const { name } = props.user;
+  return (
+    <div>
+      <h2>Another return</h2>
+      <h2>{name}</h2>
+    </div>
+  );
 };
 
 export default ShortCircuitExamples;
