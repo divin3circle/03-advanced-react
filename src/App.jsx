c7a8b426fcaef833b61120b6eabbc6b1698c550d
Sylus Abel <119014867+divin3circle@users.noreply.github.com>
2023-05-11 09:18:46 +0300

    form-data-api

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import ControlledInputs from "./tutorial/06-forms/starter/03-multiple-inputs";
+import ControlledInputs from "./tutorial/06-forms/starter/05-form-data";
 
 function App() {
   return (
